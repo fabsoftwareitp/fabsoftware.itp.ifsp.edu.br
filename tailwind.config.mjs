713ac1f33f8e43ad0585b22cb29594c7a1508af2
Leo2828 <leoleohenrique601@gmail.com>
2024-05-21 14:43:40 -0300

    new_layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		extend: {
+			colors: {
+				primary: 'fe4e00' ,
+				secondary: '03071E' ,
+				dark: '252A34' ,
+				light: 'EAEAEA' ,
+			},
+			backgroundImage: {
+				'hero-pattern':
+				  "linear-gradient(to right bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.7)), url('https://images.pexels.com/photos/270360/pexels-photo-270360.jpeg?auto=compress%20cs=tinysrgb&w=1260&h=750&dpr=1')",
+			},
+		},
+		fontFamily: {
+			'sans': ['Open Sans', 'ui-sans-serif', 'system-ui']
+		}
+	},
+	plugins: [require("@tailwindcss/typography")],
+}
