3efd74fbff1f2ed49609f1799d24e07e9d513b9c
danilocbueno <danilocbueno@gmail.com>
2024-10-10 08:31:39 -0300

    fixing import

--- generate_report.js
+++ generate_report.js
@@ -1,5 +1,5 @@
-const { Octokit } = require("@octokit/rest");
-const fs = require("fs");
+import { Octokit } from "@octokit/rest";
+import fs from "fs";
 
 // Configurações do Octokit
 const octokit = new Octokit({
